5226d32dfe997e5b3f71102e11b55980cade8308
otkur <abdulhekem@gmail.com>
2024-03-18 00:08:14 +0300

    Given two strings a and b, return the length of the longest uncommon subsequence between a and b. If no such uncommon subsequence exists, return -1.

---
+++ src/main/java/longestUncommonSubsequence.java
@@ -0,0 +1,8 @@
+public class longestUncommonSubsequence {
+
+    public  static int findLUSlength(String a, String b) {
+        if(a.equals(b))return -1;
+        else if (a.length()>b.length())return a.length();
+        else return b.length();
+    }
+}
