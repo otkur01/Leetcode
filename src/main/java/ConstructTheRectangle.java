4d21798fd2d6fad0df0afd26e74834e92b64f1e7
otkur <abdulhekem@gmail.com>
2024-03-17 17:24:48 +0300

    web developer needs to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
    The area of the rectangular web page you designed must equal to the given target area.
    The width W should not be larger than the length L, which means L >= W.
    The difference between length L and width W should be as small as possible.
    Return an array [L, W] where L and W are the length and width of the web page you designed in sequence.

--- src/main/java/ConstructTheRectangle.java
+++ src/main/java/ConstructTheRectangle.java
@@ -1,2 +1,23 @@
+import java.util.Arrays;
+
 public class ConstructTheRectangle {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(constructRectangle(122122)));
+    }
+
+    public static int[] constructRectangle(int area) {
+        int len = 0;
+        int wid = 0;
+        int st = (int) Math.sqrt(area);
+        for (int i = st; i > 0; i--) {
+            if (area % i == 0) {
+                wid = area / i;
+                len = i;
+                break;
+            }
+        }
+        int [] result = {wid,len};
+      return result;
+    }
+
 }
