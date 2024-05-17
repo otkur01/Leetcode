07bb3fa61750604d5377d13ee23d101e2c3f160a
otkur <abdulhekem@gmail.com>
2024-05-17 22:11:01 +0300

    done with sun set problem

--- src/main/java/SunRiseBuilding.java
+++ src/main/java/SunRiseBuilding.java
@@ -1,38 +1,43 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Stack;
 
 public class SunRiseBuilding {
 
     public static void main(String[] args) {
-       int [] nums = {3,5,4,1,2};
+       int [] nums = {3,5,4,4,3,1,3,2};
         System.out.println(sunRise(nums,"EAST"));
+        System.out.println(sunRise(nums,"WEST"));
 
 
     }
 
-    public static Stack<Integer> sunRise(int [] buildings, String direction) {
+    public static ArrayList<Integer> sunRise(int [] buildings, String direction) {
         Stack<Integer> integerStack = new Stack<>();
         switch (direction) {
             case "EAST":
 
-                for (int i = 0; i < buildings.length - 1; i++) {
-                    integerStack.push(i);
-                    if (buildings[i] < buildings[i + 1]) {
-                        integerStack.pop();
-                        i++;
-                    }
+                for (int i = 0; i < buildings.length; i++) {
 
+                    while ( !integerStack.isEmpty() && buildings[integerStack.peek()] <= buildings[i] ) {
+                        integerStack.pop();
+
+                    }
+                    integerStack.push(i);
 
                 }
                 break;
             case "WEST":
-                integerStack.push(buildings[buildings.length - 1]);
-                for (int i = buildings.length - 2; i > 0; i--) {
-                    if (buildings[i] > buildings[i - 1]) {
-                        integerStack.push(i);
+                for (int length = buildings.length-1; length >=0; length--) {
+
+                    while ( !integerStack.isEmpty() && buildings[integerStack.peek()] <= buildings[length] ) {
+                        integerStack.pop();
 
                     }
-                    break;
+                    integerStack.push(length);
+
+                }
+                break;
 
                 }
 
@@ -40,9 +45,9 @@ public class SunRiseBuilding {
 //        while (!integerStack.isEmpty()){
 //            result.add(integerStack.peek());
 //            integerStack.pop();
-        }
 
-                return integerStack;
+                if (direction.equalsIgnoreCase("west")) Collections.reverse(integerStack);
+                return new ArrayList<>(integerStack);
 
     }
 }
