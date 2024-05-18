cb76ed6687d9ed6dbd59392f781a263d4d6e79cf
otkur <abdulhekem@gmail.com>
2024-05-18 22:47:48 +0300

    You are given the heads of two sorted linked lists list1 and list2.
    Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
    
    Return the head of the merged linked list.

--- src/main/java/FindCylcleSLL.java
+++ src/main/java/FindCylcleSLL.java
@@ -1,3 +1,5 @@
+
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -6,26 +8,35 @@ public class FindCylcleSLL {
     public class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
         }
 
     }
-    public boolean hasCycle(ListNode head) {
-        Set<ListNode>set = new HashSet<>();
-        ListNode current = head;
-        while (current!=null){
-           if(set.contains(current))return true;
-           set.add(current);
-           current=current.next;
-        }
 
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode current = head;
+        while (current != null) {
+            if (set.contains(current)) return true;
+            set.add(current);
+            current = current.next;
+        }
 
 
         return false;
 
     }
+
     public boolean hasCycle2(ListNode head) {
         if (head == null) return false;
 
@@ -51,6 +62,42 @@ public class FindCylcleSLL {
         return temp2 != null && temp2.next != null;
     }
 
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = head;
+        ListNode current = head;
 
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            current = nextNode;
+
+
+        }
+        return head;
+
+
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode tempHead = new ListNode();
+        ListNode prev = tempHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                prev.next = list1;
+                list1 = list1.next;
+            } else {
+                prev.next = list2;
+                list2 = list2.next;
+            }
+            prev = prev.next;
+        }
+        if(list1==null)prev.next=list2;
+        else prev.next=list1;
+
+        return tempHead.next;
+
+    }
+
+}
 
 }
