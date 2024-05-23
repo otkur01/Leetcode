b3efc9fda355d4bf599c6b9cbda85db07c307f33
otkur <abdulhekem@gmail.com>
2024-05-23 11:05:26 +0300

    two pointer solutoins

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -25,17 +25,17 @@ public class InsertPosition {
     }
 
     public static int searchInsert1(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
+        int s = 0;
+        int e = nums.length-1;
 
-        while (start <= end) {
-            int mid = start + (end-start)/2;
+        while (s <= e) {
+            int mid = s + (e-s)/2;
             if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) end = mid-1;
-            else start = mid+1;
+            else if (nums[mid] > target) e = mid-1;
+            else s = mid+1;
         }
 
-        return start;
+        return s;
     }
 
 
