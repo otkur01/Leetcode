a1995bcf60708bc60eb5b3aa901108009a92b17b
otkur <abdulhekem@gmail.com>
2024-06-09 19:34:06 +0300

    BST search

--- src/main/java/TreeNode.java
+++ src/main/java/TreeNode.java
@@ -1,87 +1,70 @@
- public class TreeNode {
-  int val;
-  TreeNode left;
-  TreeNode right;
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-  TreeNode() {
-  }
-
-  TreeNode(int val) {
-   this.val = val;
-  }
-
-  TreeNode(int val, TreeNode left, TreeNode right) {
-   this.val = val;
-   this.left = left;
-   this.right = right;
-  }
-  public TreeNode insertIntoBST(TreeNode root, int val) {
-   TreeNode newNode = new TreeNode(val);
-
-   if(root==null) {
-    root = newNode;
-    return root;
-   }
-
-   TreeNode current = root;
-
-   while (true){
-       if (val<current.val){
-        if(current.left==null) {
-         current.left = newNode;
-         return root;
-        }
-        current=current.left;
-
-       }else {
-        if (current.right==null) {
-         current.right = newNode;
-          return root;
-        }
-
-        current=current.right;
-       }
-
-
-   }
-
-
-  }
-
-  public TreeNode searchBST(TreeNode root, int val) {
-   TreeNode newNode = new TreeNode(val);
-
-   if(root==null) {
-    root = newNode;
-    return root;
-   }
-
-   TreeNode current = root;
-
-   while (true){
-    if (val<current.val){
-     if(current.left==newNode) {
-
-      return current;
-     }
-     current=current.left;
-
-    }else {
-     if (current.right==newNode) {
-
-      return current;
-     }
-
-     current=current.right;
+    TreeNode() {
     }
 
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        TreeNode newNode = new TreeNode(val);
+
+        if (root == null) {
+            root = newNode;
+            return root;
+        }
+
+        TreeNode current = root;
+
+        while (true) {
+            if (val < current.val) {
+                if (current.left == null) {
+                    current.left = newNode;
+                    return root;
+                }
+                current = current.left;
+
+            } else {
+                if (current.right == null) {
+                    current.right = newNode;
+                    return root;
+                }
+
+                current = current.right;
+            }
 
 
+        }
 
 
-   }
+    }
 
+    public TreeNode searchBST(TreeNode root, int val) {
+        TreeNode newNode = new TreeNode(val);
 
+        if (root == null) {
+            return null;
+        }
 
+        TreeNode current = root;
 
- }
+        while (current != null) {
+            if (val > current.val) current = current.right;
+            else if (val < current.val) current = current.left;
+            else return current;
+
+        }
+        return null;
+    }
+
+}
